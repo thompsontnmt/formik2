fb67e2c1460b5907f8d6941c6e664a842b0577c9
Mike Thompson <mtthompson11@gmail.com>
2023-03-14 22:50:31 -0500

    more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Formik, Field, Form, useField, FieldAttributes } from 'formik'
+import { Button, Checkbox, FormControlLabel, Radio, TextField } from '@material-ui/core'
+// import { number } from 'yup';
+// import * as Yup from 'yup';
+// import 'yup-phone';
 
-function App() {
+type MyRadioProps = { label: string } & FieldAttributes<{}>
+
+const MyRadio: React.FC<MyRadioProps> = ({label, ...props}) => {
+  const [field] = useField<{}>(props);
+  return <FormControlLabel {...field} control={<Radio />} label={label} />;
+};
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formik initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        mobile: '',
+        contact: '',
+        agreement: false,
+      }}
+      onSubmit={(data, {setSubmitting}) => {
+        setSubmitting(true);
+        //make async call
+        console.log('submit: ', data);
+        setSubmitting(false)
+      }}
+      >
+        {({ values, isSubmitting }) => (
+
+          <Form>
+            <Field
+              placeholder='First Name'
+              name="firstName"
+              type='input'
+              as={TextField}
+            />
+          <div>
+            <Field
+              placeholder='Last Name'
+              name="lastName"
+              type='input'
+              as={TextField}
+            />
+          </div>
+          <div>
+            <Field
+              placeholder='Email'
+              name="email"
+              type='input'
+              as={TextField}
+            />
+          </div>
+          <div>
+            <Field
+              placeholder='Mobile'
+              name="mobile"
+              type='input'
+              as={TextField}
+            />
+          </div>
+            <Field 
+              name='agreement'
+              type='checkbox'
+              as={Checkbox}
+            />
+
+            
+          <div>Preferred Method of Contact</div>
+            <MyRadio name='contact' type='radio' value='call' label='call' />
+            <MyRadio name='contact' type='radio' value='text' label='text' />
+            <MyRadio name='contact' type='radio' value='email' label='email' />
+          <div>
+            <Button type='submit'>Submit</Button>
+          </div>
+          <pre>{JSON.stringify(values, null, 2)}</pre>
+        </Form>
+          )}
+
+      </Formik>
     </div>
   );
-}
+};
 
-export default App;
+export default App
